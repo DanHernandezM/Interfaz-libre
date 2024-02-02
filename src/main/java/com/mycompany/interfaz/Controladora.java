0438f76d96086e55bbaa477b4a824b2ac2aa973a
nenod <nenod@Acer_NDHM>
2024-02-01 22:00:45 -0600

    Ya no me acuerdo como subir al git :c

---
+++ src/main/java/com/mycompany/interfaz/Controladora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.interfaz;
+
+/**
+ *
+ * @author nenod
+ */
+public class Controladora {
+    public static void main(String[] args) {
+        // eso es todo? PUES SI por fa denme de baja
+      Interfaz buzon = new Interfaz();
+        buzon.setVisible(true);
+    }    
+}
